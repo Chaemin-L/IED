f66a4ef0584520ff3042d2526a04221e1991f2fb
Chaemin-Lee <chaem12@kookmin.ac.kr>
2020-09-18 02:26:42 +0900

    GPIO를 통한 LED 제어

--- _05_practice_2.ino
+++ _05_practice_2.ino
@@ -1,11 +1,33 @@
-#define PIN_LED 13
+#define PIN_LED 7
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
 }
 
 void loop() {
-    digitalWrite(PIN_LED, 0);
-    //digitalWrite(PIN_LED, 1);    
+  digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED,1);
+  while(1);
 }
 
